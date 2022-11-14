0be47bf3a3711c1abb8b8f3b651576a11ed00b20
Patrick C <patrickcaldwell1998@gmail.com>
2022-11-14 16:02:44 -0600

    Removed underline in project tools

--- application/views/projects.php
+++ application/views/projects.php
@@ -4,21 +4,21 @@
             <div class="col">
                 <div class="tool">
                     <div class="tool__logo"><i class="fa-brands fa-php"></i></div>
-                    <div class="tool__title lgFont underline">Development</div>
+                    <div class="tool__title lgFont">Development</div>
                     <div class="tool__desc lgFont">From simple webpages to complex websites and webapps.</div>
                 </div>
             </div>
             <div class="col">
                 <div class="tool">
                     <div class="tool__logo"><i class="fa-solid fa-database"></i></div>
-                    <div class="tool__title lgFont underline">Databases</div>
+                    <div class="tool__title lgFont">Databases</div>
                     <div class="tool__desc lgFont">Both MySQL and NoSQL database design, creation, and management.</div>
                 </div>
             </div>
             <div class="col">
                 <div class="tool">
                     <div class="tool__logo"><i class="fa-solid fa-pencil"></i></div>
-                    <div class="tool__title lgFont underline">Design</div>
+                    <div class="tool__title lgFont">Design</div>
                     <div class="tool__desc lgFont">Experience with the design process and Nielsen heuristics to design products.</div>
                 </div>
             </div>
