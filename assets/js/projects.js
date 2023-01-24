98e3757e5c5cefd476d969d25cd01b91ed532476
Patrick C <patrickcaldwell1998@gmail.com>
2023-01-24 16:21:44 -0600

    Fixed Project Searching CSS

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,9 +1,11 @@
 function searchProjects(terms){
+    
     let projects = document.getElementsByClassName("project");
+    console.log(projects);
     
     if (!terms){ //Show all projects
         for(let i = 0; i < projects.length; i++){
-            projects[i].parentElement.style.display = "block";
+            projects[i].parentElement.parentElement.style.display = "block";
         }
         return;
     }
@@ -21,9 +23,9 @@ function searchProjects(terms){
         terms = terms.toUpperCase();
         terms = terms.trim();
         if(projName.search(terms)==-1 && projTags.search(terms)==-1){
-            projects[i].parentElement.style.display = "none";
+            projects[i].parentElement.parentElement.style.display = "none";
         }else{
-            projects[i].parentElement.style.display = "block";
+            projects[i].parentElement.parentElement.style.display = "block";
         }
     }
 
